5ed8903e09066f1bd029b7d3fe094ed4f7fc180a
Hacker1127 <105992023+Hacker1127@users.noreply.github.com>
2022-05-21 23:11:00 +0300

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,95 @@
+#!/user/bin/evn python
+import os
+
+
+os.system("clear")
+
+yesil = '\033[92m'
+kirmizi = '\033[91m'
+beyaz = '\033[37m'
+sari = '\033[93m'
+
+os.system("pkg install python")
+os.system("pkg install git")
+os.system("pkg install figlet")
+os.system(" clear")
+print( sari +"""
+_____________________________/__________________________________________
+_____    ___     ___    _       ____
+|_   _|  / _ \   / _ \  | |     / ___|
+  | |   | | | | | | | | | |     \___ \
+  | |   | |_| | | |_| | | |___   ___) |
+  |_|    \___/   \___/  |_____| |____/
+
+--------------------------------------------------------+------------------
+
+        1-) instegram crack
+
+        2-)watsap spam bot
+
+        3-) instegram banned
+
+        4-) instegram takipçi
+
+        5-) hakımızda
+
+        6-)instegram fake panel
+
+        7-) ÜSTÜN bilgi toplama aracı
+
+        8-)exit
+""")
+
+işlemno =input("lütfen işlemno seçiniz : ")
+
+if işlemno == "1" :
+        os.system("clear")
+        os.system("figlet ∆ ")
+        os.system("git clone https://github.com/rohamasadi/instagramCracker")
+        os.system(" cd instagramCracker")
+        os.system(" python tool.py")
+
+elif işlemno == "2" :
+        os.system("clear")
+        os.system("figlet R - I R A ")
+        os.system("git clone git clone https://github.com/sandiwijayani1/SpamWa-1")
+        os.system("clear")
+        os.system("python tool.py")
+
+elif işlemno == "3" :
+        print(  kirmizi +  """
+
+        üzgünüz  3 numaralı sayfaya  erişim izniniz bulunamadı """)
+
+
+elif işlemno == "4" :
+        os.system ("clear")
+        os.system("git clone https://github.com/officialputuid/toolsig")
+        os.system("clear")
+        os.system("pkg install sl")
+        os.system("sl")
+        os.system("python tool.py")
+
+elif işlemno == "5" :
+        os.system("clear")
+        os.system("python tool.py")
+
+elif işlemno == "7" :
+        os.system("pkg install  git")
+        os.system("pip install git")
+        os.system("git clone https://github.com/Moham3dRiahi/Th3inspector")
+        os.system("python tool.py")
+elif işlemno =="6" :
+
+        os.system("clear")
+        os.system ("pkg install git")
+        os.system("pkg install curul")
+        os.system ("git cline git clone https://github.com/htr-tech/nexphisher")
+elif işlemno == "8" :
+        os.system ("clear")
+        os.system("exit")
+
+else :
+        print(kirmizi + """
+        kör gözünü s*kiym
+        """)
